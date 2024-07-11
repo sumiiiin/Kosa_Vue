1ef252d8abb5ba96830aa635ea991c7b9f0d74ff
soo <hongmoon0430@gmail.com>
2024-07-11 22:02:53 +0900

    added coment

--- src/router/index.js
+++ src/router/index.js
@@ -24,10 +24,13 @@ const routes = [
 ]
 // 구조분해하여 다른 라우터 파일 추가
 
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  //  historyAPI를 사용하여 라우터의 히스토리를 사용하여 URL을 관리할 수 있다.
+  // 환경 변수로 설정된 기본 URL을 사용하여 라우터를 초기화
+  // SPA가 서브 디렉토리에서 호스팅될 경우 초기 경로에서 부터 시작.
+
+  routes // 라우트 구성을 나타내고
 })
 
 export default router
